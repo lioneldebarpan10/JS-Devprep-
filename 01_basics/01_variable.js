231fcee83659be6e77ae3cfda254d15b7cded0ce
lioneldebarpan10 <161072133+lioneldebarpan10@users.noreply.github.com>
2025-01-14 19:56:07 +0000

    Lecture -1 Variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountID = 12345
+let accountEmail = "dbrpn07cse@gmail.com"
+var accountPassword = "14454"
+
+accountcity = "Jaipur"
+
+// accountID = 2; Not allowed
+console.log(accountID)
+

acc462cb35c640af780af43fba122efd89a2cb30
lioneldebarpan10 <161072133+lioneldebarpan10@users.noreply.github.com>
2025-01-15 15:54:16 +0000

    Updated Basics(Variables) folder

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,9 +1,15 @@
 const accountID = 12345
-let accountEmail = "dbrpn07cse@gmail.com"
-var accountPassword = "14454"
+let accountEmail = "dbrpn07@gmail.com" //You can use Scope (Lexial scope, Block Scope ,functional scope)
+var accountPassword = "14454" //You cannot use Scope using Var
 
-accountcity = "Jaipur"
+/*Prefer not to use Var because of issue in block scope and functional scope*/
 
-// accountID = 2; Not allowed
+accountCity = "Jaipur"
+let accountState
+
+//accountID = 2; Not allowed to rewrite a constant
+accountEmail = "dbrpn06@gmail.com"
 console.log(accountID)
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState])
+
 

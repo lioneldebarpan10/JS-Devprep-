23b118c7f2c4ce766e3a2b1a62bcd6b042389fd7
lioneldebarpan10 <161072133+lioneldebarpan10@users.noreply.github.com>
2025-01-15 20:17:03 +0000

    Updated 03 and Created and Updated 04

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -20,5 +20,33 @@ console.log(booleanisloggedin);
 let someNumber = 33
 let stringnumber = String(someNumber)
 console.log(stringnumber);
-console.log(typeof stringnumber)
+console.log(typeof stringnumber) //String
 
+/****Operations****/
+ let value = 5
+ let negvalue = -value
+ console.log(negvalue)
+//  console.log(2+2)
+//  console.log(5-2)
+//  console.log(5*2)
+//  console.log(25/2)
+//  console.log(2**3)
+//  console.log(12%5)
+
+let str1  = "Hello"
+let str2  = "Abir"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1"+2+2) // 122
+console.log(1+2+"2") // 32
+
+// console.log(+true) ans => 1
+// console.log(+"")   ans => 0
+
+let gamecounter = 100
+gamecounter++ ;
+console.log(gamecounter)
+++gamecounter
+console.log(gamecounter)

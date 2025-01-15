33cbaeed905602cf45381152bffead7e954d66cc
lioneldebarpan10 <161072133+lioneldebarpan10@users.noreply.github.com>
2025-01-15 20:31:39 +0000

    Updated 04_comparison in Js

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,18 +4,16 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
+// console.log("2" > 1); Ans - True
+// console.log("02" > 1); Ans - True
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log(null > 0);  //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+console.log(undefined == 0); //false
+console.log(undefined > 0);  //false
+console.log(undefined < 0);  //false
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
-
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
-
-// === 
+// '===' => strictly Equal to
 
 console.log("2" === 2);

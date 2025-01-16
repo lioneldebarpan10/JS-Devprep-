e2d39e60ab73d0f07efb7ced765bd0c2967219db
Debarpan Deb <161072133+lioneldebarpan10@users.noreply.github.com>
2025-01-16 16:03:38 +0000

    Updated Data Types Summary

--- 01_basics/datatypessummary.js
+++ 01_basics/datatypessummary.js
@@ -22,7 +22,7 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ["Superman", "Batman", "Iron Man"];
 let myObj = {
     name: "hitesh",
     age: 22,
@@ -35,3 +35,23 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let myYoutubename = "hiteshchowdhury.com"
+
+let anothername = myYoutubename
+anothername = "Chai aur Code"
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user1gmail.com",
+    UPI:"user@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "abir06gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
